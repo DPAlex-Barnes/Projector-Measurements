1725235ff2da2a6afe4a03c34aba6d0f39590899
abarnes <abarnes@UKMANNB017>
2018-07-20 15:31:35 +0100

    Created Minolta Object and moved related process over

--- Projector Measurements/Form1.cs
+++ Projector Measurements/Form1.cs
@@ -16,25 +16,21 @@ namespace Projector_Measurements
 {
     public partial class Form1 : Form
     {
-        char[] receptor = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-        char[] command54 = { '5', '4', '1', ' ', ' ', ' ' };
-        char[] command55 = { '5', '5', '0', ' ', ' ', '1' };
-        char[] command10 = { '1', '0', '0', '2', '0', '1' };
-        
+        MinoltaRig minolta = new MinoltaRig();
+        bool isConnected = false;
 
-        SerialPort port;
-        string[] comms = SerialPort.GetPortNames();
+
         double Wuxga = 4.56;
         double[] ninePoint = new double[9] { 699, 663, 668, 513, 595, 559, 673, 529, 541 };
-        string[] lxData = new string[9] { "+ 3485","+23455","+54365","+23435","+   55","+   65","+ 6545","+45655","+  235"};
-        
+        string[] lxData = new string[9] { "+ 3485", "+23455", "+54365", "+23435", "+   55", "+   65", "+ 6545", "+45655", "+  235" };
+
 
 
         public Form1()
         {
             InitializeComponent();
-            ComboBoxComms.Items.AddRange(comms);
-            
+            ComboBoxComms.Items.AddRange(minolta.comms);
+
 
 
         }
@@ -42,8 +38,8 @@ namespace Projector_Measurements
 
         private void button1_Click(object sender, EventArgs e)
         {
-            ninePoint = ProcessData(lxData);
-       
+            ninePoint = ProcessData(minolta.Read());
+
             string reading = Convert.ToString(Calculations.Lumens(ninePoint, Wuxga));
             TopLeft.Text = Convert.ToString(ninePoint[0]);
             Top.Text = Convert.ToString(ninePoint[1]);
@@ -54,9 +50,9 @@ namespace Projector_Measurements
             BottomLeft.Text = Convert.ToString(ninePoint[6]);
             Bottom.Text = Convert.ToString(ninePoint[7]);
             BottomRight.Text = Convert.ToString(ninePoint[8]);
-            
+
             LumensRead.Text = reading;
-      
+
         }
 
         #endregion
@@ -65,113 +61,23 @@ namespace Projector_Measurements
 
         private void BtnConnect_Click(object sender, EventArgs e)
         {
-            var item = (string)ComboBoxComms.SelectedItem;
-
-            byte[] bytestosend = sendCommand(command54, receptor[0], receptor[0]);
-            bool isConnected = false;
-            try
-            {
-                port = new SerialPort(item, 9600, Parity.Even, 7, StopBits.One);
-                port.Open();
-                port.Write(bytestosend, 0, bytestosend.Length);
-
-                isConnected = true;
-                label1.Text = port.ReadLine();
-
-            }
-            catch (Exception)
-            {
-                label1.Text = "No Connection";
-            }
-
+            bool Complete = false;
+            label1.Text = "Connecting...";
+            isConnected = minolta.Initialise((string)ComboBoxComms.SelectedItem);
+            
             if (isConnected)
             {
-                var read = Task.Run(() =>
-                {
-                    port.Write(sendCommand(command55, receptor[9], receptor[9]), 0, 14);
-                    Thread.Sleep(80);
-
-                    for (int i = 1; i <= 9; i++)
-                    {
-                        port.Write(sendCommand(command10, receptor[0], receptor[i]), 0, 14);
-                        Thread.Sleep(80);
-                    }
-                    Thread.Sleep(5000);
-                    
-
-                    for (int i = 0; i < 9; i++)
-                    {
-                        port.Write(sendCommand(command10, receptor[0], receptor[i+1]), 0, 14);
-                        Console.WriteLine("Result:" + receptor[i+1] + " " + port.ReadLine());
-                        lxData[i] = port.ReadLine().Substring(9,6);
-                        Thread.Sleep(80);
-                    }
-                    Invoke(new Action(() =>
-                    {
-                        label3.Text = "Done";
-                    }));
-
-                });
+                Complete = minolta.Start();
             }
-            
-        }
-
-        #endregion
-
-        #region Connection functions
-
-        public static char[] Bcc(char[] command, char rec10, char rec1)
-        {
-            
-            int bcc = rec10;
-            bcc ^= rec1;
-            foreach (char item in command)
+            if (Complete)
             {
-                bcc ^= item;
-            }
-            bcc ^= 0x03;
+                label1.Text = "Connected";
 
-            string s = bcc.ToString("x2");
-            var array = s.Split();
-            return s.ToCharArray();   
+            }
         }
+    
 
-        public static byte[] sendCommand(char[] command, char rec10, char rec1)
-        {
-            char[] bcc = Bcc(command, rec10, rec1);
-
-            // Command Constants
-            byte STX = 0x02;
-            byte ETX = 0x03;
-            byte CR = 0x0D;
-            byte LF = 0x0A;
-
-            // Converting Char to Byte
-            byte[] commandByte = new byte[command.Length];
-            byte rec10Byte = (byte)rec10;
-            byte rec1Byte = (byte)rec1;
-
-            for (int i = 0; i < command.Length; i++)
-            {
-                commandByte[i] = (byte)command[i];
-            }
-                      
-            // build complete command
-            byte[] complete = { STX, rec10Byte, rec1Byte, 0, 0, 0, 0, 0, 0, ETX,
-                (byte)bcc[0], (byte)bcc[1], CR, LF};
-
-            for (int i = 0; i< commandByte.Length; i++)
-            {
-                
-                complete[i + 3] = commandByte[i];
-
-            }
-
-            return complete;
-        }
-
-
-        #endregion
+    #endregion
 
         public static double[] ProcessData(string[] data)
         {
---
+++ Projector Measurements/MinoltaRig.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projector_Measurements
+{
+    class MinoltaRig
+    {
+        SerialPort port;
+        public string[] comms = SerialPort.GetPortNames();
+        private char[] receptor = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        private char[] command54 = { '5', '4', '1', ' ', ' ', ' ' };
+        private char[] command55 = { '5', '5', '0', ' ', ' ', '1' };
+        private char[] command10 = { '1', '0', '0', '2', '0', '1' };
+
+        public bool Initialise(string CommPort)
+        {
+
+
+            byte[] bytestosend = sendCommand(command54, receptor[0], receptor[0]);
+            bool isConnected = false;
+            try
+            {
+                port = new SerialPort(CommPort, 9600, Parity.Even, 7, StopBits.One);
+                port.Open();
+                port.Write(bytestosend, 0, bytestosend.Length);
+                
+             
+                if (port.ReadLine().Contains("0054"))
+                {
+                    isConnected = true;
+
+                }
+                
+
+            }
+            catch (Exception)
+            {
+                isConnected = false;
+            }
+
+            return isConnected;
+        }
+
+        public bool Start()
+        {
+            bool isComplete = false;
+            var set = Task.Run(() =>
+            {
+                port.Write(sendCommand(command55, receptor[9], receptor[9]), 0, 14);
+                Thread.Sleep(80);
+
+                for (int i = 1; i <= 9; i++)
+                {
+                    port.Write(sendCommand(command10, receptor[0], receptor[i]), 0, 14);
+                    Thread.Sleep(80);
+                    
+                }
+                Thread.Sleep(5000);
+                isComplete = true;
+            });
+            while (!isComplete)
+            {
+                
+            }
+            isComplete = true;
+            Console.WriteLine(isComplete);
+            return isComplete;
+        }
+
+        public string[] Read()
+        {
+            bool isRunning = true;
+            string[] lxData = new string[9];
+            var read = Task.Run(() =>
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    port.Write(sendCommand(command10, receptor[0], receptor[i + 1]), 0, 14);
+                    Console.WriteLine("Result:" + receptor[i + 1] + " " + port.ReadLine());
+                    lxData[i] = port.ReadLine().Substring(9, 6);
+                    Thread.Sleep(80);
+                }
+                Thread.Sleep(5000);
+                isRunning = false;                           
+            });
+            while (isRunning)
+            {
+                
+            }
+
+            return lxData;
+
+        }
+
+        #region Creating command to send to the Minolta
+        private char[] Bcc(char[] command, char rec10, char rec1)
+        {
+
+            int bcc = rec10;
+            bcc ^= rec1;
+            foreach (char item in command)
+            {
+                bcc ^= item;
+            }
+            bcc ^= 0x03;
+
+            string s = bcc.ToString("x2");
+            var array = s.Split();
+
+            return s.ToCharArray();
+        }
+
+        
+
+        private byte[] sendCommand(char[] command, char rec10, char rec1)
+        {
+            char[] bcc = Bcc(command, rec10, rec1);
+
+            // Command Constants
+            byte STX = 0x02;
+            byte ETX = 0x03;
+            byte CR = 0x0D;
+            byte LF = 0x0A;
+
+            
+            byte[] commandByte = toByte(command);
+
+            // build complete command
+            byte[] complete = { STX, toByte(rec10), toByte(rec1), 0, 0, 0, 0, 0, 0, ETX,
+                (byte)bcc[0], (byte)bcc[1], CR, LF};
+
+            for (int i = 0; i < commandByte.Length; i++)
+            {
+
+                complete[i + 3] = commandByte[i];
+
+            }
+
+            return complete;
+        }
+
+        private byte[] toByte(char[] sArray)
+        {
+            byte[] bArray = new byte[sArray.Length];
+            
+            for (int i = 0; i < sArray.Length; i++)
+            {
+                bArray[i] = (byte)sArray[i];
+            }
+            return bArray;
+        }
+
+        private byte toByte(char c)
+        {
+
+            return (byte)c;
+        }
+
+        #endregion
+
+    }
+}
+
+
+
+        
+    
+
+    
+
