582920fbca1a9fb0ce80f6a136e2dc91b5077db4
abarnes <abarnes@UKMANNB017>
2018-07-20 11:53:01 +0100

    Add project files.

---
+++ Projector Measurements/Calculations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projector_Measurements
+{
+    class Calculations
+    {
+        public static double Lumens(double[] reading, double area)
+        {
+            double mean = reading.Average();
+            int lumens = Convert.ToInt32(mean * area);
+            return lumens;
+        }
+
+        
+    }
+}
